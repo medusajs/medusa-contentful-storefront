44d28234670302625440ccdce9fd7bdc882beeee
Sebastian Rindom <skrindom@gmail.com>
2021-08-19 11:39:34 +0200

    skeleton

---
+++ src/utils/zero-decimal-currencies.js
@@ -0,0 +1,21 @@
+const zeroDecimalCurrencies = [
+  "bif",
+  "clp",
+  "djf",
+  "gnf",
+  "jpy",
+  "kmf",
+  "krw",
+  "mga",
+  "pyg",
+  "rwf",
+  "ugx",
+  "vnd",
+  "vuv",
+  "xaf",
+  "xof",
+  "xpf",
+]
+
+export default zeroDecimalCurrencies
+
